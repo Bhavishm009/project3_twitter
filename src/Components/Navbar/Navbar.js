167285b374e6e2c8a914f3cc45f41675c255171e
Bhavishm009 <bhavishm009@gmail.com>
2023-02-11 20:17:30 +0530

    Added Drawer

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,18 +2,20 @@ import React from "react";
 import style from "./Navbar.module.css";
 import { FaTwitter } from "react-icons/fa";
 import { Avatar } from "@mui/material";
+import AppDrawer from "../../Atom/Drawer/Drawer";
 function Navbar() {
   return (
     <>
       <div className={style.feed__header}>
         <h2>Home</h2>
         <div className={style.mobicon}>
-          <Avatar
+          <AppDrawer><Avatar
             alt="Remy Sharp"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBhcplevwUKGRs1P-Ps8Mwf2wOwnW_R_JIA&usqp=CAU"
-          />
+          /> </AppDrawer>
 
           <FaTwitter className={style.logo} />
+
         </div>
 
         <div className={style.innercontainer}>
